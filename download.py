0a80dc62cb6a30f30679b0b72650d4a3212eaf7a
szlegp <szlegp@gmail.com>
2022-11-21 11:49:14 +0100

    GPU data.

--- download.py
+++ download.py
@@ -19,10 +19,14 @@ def key_to_classes(key, row, fields_classes: defaultdict[list]):
         return len(classes)-1
 
 NUMBER = {"ModelEntity" : ["ramAmount", "driveStorage", "weight", "ramNumberOfFreeSlots",
-"ramMaxAmount", "hddSpeed"], "BenchmarkEntity":["benchmark"], "ProcessorEntity": ["cores", "frequency"],
+"ramMaxAmount", "hddSpeed"], ("ProcessorEntity", "benchmark_entity"):["benchmark"], 
+("ProcessorEntity", "benchmark_entity"):["benchmark"], 
+("GraphicsEntity", "benchmark_entity"):["benchmark"], 
+"ProcessorEntity": ["cores", "frequency"],
 "ScreenEntity":["refreshRate", "diagonalScreenInches"]}
 CATEGORICAL = {"ModelEntity" : ["color", "ramType", "driveType"],
-"ScreenEntity":["screenFinish"], "ScreenEntity":["touchScreen"]
+"ScreenEntity":["screenFinish"], "ScreenEntity":["touchScreen"],
+"GraphicsEntity":["graphicsCardVRam"]
 }
 
 def get_data():
@@ -36,17 +40,26 @@ def get_data():
     X_pre = []
     Y = []
     
-    for row in (session.query(OfferEntity, ModelEntity, ProcessorEntity,BenchmarkEntity,ScreenEntity)
+    for row in (session.query(OfferEntity, ModelEntity, ProcessorEntity, GraphicsEntity,ScreenEntity)
     .filter(OfferEntity.modelId==ModelEntity.id)
     .filter(ModelEntity.processorId==ProcessorEntity.id)
-    .filter(ProcessorEntity.benchmarkId==BenchmarkEntity.id)
+    .filter(ModelEntity.graphicsId==GraphicsEntity.id)
     .filter(ModelEntity.screenId==ScreenEntity.id)
     ).limit(1000).all():
         Y.append(row.OfferEntity.offerPrice // 1000 * 1000)
         new_row = {}
         for table, fields in NUMBER.items():
             for field in fields:
-                new_row[field] = getattr(getattr(row, table), field) or 0
+                if type(table)==tuple:
+                    target = getattr(getattr(row, table[0]), table[1])
+                else:
+                    target = getattr(row, table)
+                # GPU is integrated and None
+                if target:
+                    value = getattr(target, field)
+                else:
+                    value = None
+                new_row[field] = value or 0
         for table, fields in CATEGORICAL.items():
             for field in fields:
                 new_row[field] = key_to_classes(field, getattr(row, table), fields_classes)
@@ -64,6 +77,9 @@ def get_data():
                 new_row.extend([1 if row[field]==_class else 0 
                 for _class in range(len(fields_classes[field]))])
         X.append(new_row)
+
+    # cleanup some memory
+    del X_pre, fields_classes
 
     session.close()
 
--- lr.py
+++ lr.py
@@ -9,7 +9,7 @@ from download import get_data
 X_train, X_test, y_train, y_test = train_test_split(*get_data(), test_size=0.2, random_state=0)
 
 # https://blog.bigml.com/2016/09/26/predicting-airbnb-prices-with-logistic-regression/
-classifier = LogisticRegression(multi_class='multinomial', solver='lbfgs', max_iter=200)
+classifier = LogisticRegression(multi_class='multinomial', solver='lbfgs', max_iter=1000)
 classifier.fit(X_train, y_train)
 score = classifier.score(X_test, y_test)
 
