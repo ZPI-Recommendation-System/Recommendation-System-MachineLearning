244c94f0bfe059e3b9abb6225920131fe4d6200a
szlegp <szlegp@gmail.com>
2022-11-22 11:35:05 +0100

    Kmeans print cluster centers in readable format.

--- kmeans.py
+++ kmeans.py
@@ -13,16 +13,32 @@ if DATA_FROM_DB:
 else:
    with open("data.bin", "rb") as f:
       data = pickle.load(f)
-   print("Data loaded from file")
+   # print("Data loaded from file")
 
 X, Y, fields_classes = data
 
 with open("index_to_field.bin", "rb") as f:
-   index_to_field = pickle.load(f)
+   index_to_field:dict[int, str] = pickle.load(f)
 
 kmeans = KMeans(n_clusters=12, random_state=0).fit(X)
 
+
+with open("classes.bin", "rb") as f:
+   fields_classes:dict[str, list] = pickle.load(f)
+
+print("field,class,value")
 for center in kmeans.cluster_centers_:
    print("\nCluster center:")
+   values = {}
    for i in range(len(center)):
-      print(index_to_field[i], center[i])
+      field = index_to_field[i]
+      if field in fields_classes:
+         class_start = [key for key, value in index_to_field.items() if value == field][0]
+         class_index = i - class_start
+         clss = (fields_classes[field][class_index] or "-")
+         if not field in values or values[field][1] < center[i]:
+            values[field] = (clss, center[i])
+      else:
+         values[field] = ("{:.2f}".format(center[i]), 1)
+   for key, value in values.items():
+      print(f"{key}: {value[0]}")
