4a488a3d8bcb4f5c2f61b04f240241cff5c5e9d4
szlegp <szlegp@gmail.com>
2022-12-01 12:02:45 +0100

    Training NN model.

--- nn.py
+++ nn.py
@@ -7,7 +7,10 @@ import torch
 import torch.nn as nn
 
 import matplotlib.pyplot as plt
+from pathlib import Path
 
+# create directory models if it doesn't exist using Path module
+Path("models").mkdir(parents=True, exist_ok=True)
 
 DATA_FROM_DB = False
 
@@ -30,17 +33,16 @@ X, Y = list(zip(*l))
 
 X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.4, random_state=0)
 
-new_X, new_Y = ADASYN(sampling_strategy='minority').fit_resample(X_train, y_train)
-
+X_train_balanced, Y_train_balanced = ADASYN(sampling_strategy='minority').fit_resample(X_train, y_train)
 
 class Net(nn.Module):
-    def __init__(self, D_in, H1, H2, H3, D_out):
+    def __init__(self, input_size, H1, H2, H3, output_size):
         super(Net, self).__init__()
         
-        self.linear1 = nn.Linear(D_in, H1)
+        self.linear1 = nn.Linear(input_size, H1)
         self.linear2 = nn.Linear(H1, H2)
         self.linear3 = nn.Linear(H2, H3)
-        self.linear4 = nn.Linear(H3, D_out)
+        self.linear4 = nn.Linear(H3, output_size)
         
     def forward(self, x):
         y_pred = self.linear1(x).clamp(min=0)
@@ -52,23 +54,56 @@ class Net(nn.Module):
 
 H1, H2, H3 = 500, 1000, 200
 
-numeric_x = torch.tensor(new_X, dtype=torch.float)
-numeric_y = torch.tensor([[y] for y in new_Y], dtype=torch.float)
+labels = list(range(1000, 11000, 1000))
+print("labels", labels)
 
-D_in, D_out = numeric_x.shape[1], numeric_y.shape[1]
+X_train_tensor = torch.tensor(X_train_balanced, dtype=torch.float)
+Y_train_to_classes = [[1 if y == label else 0 for label in labels]
+           for y in Y_train_balanced]
+Y_train_tensor = torch.tensor(Y_train_to_classes, dtype=torch.float)
 
-model1 = Net(D_in, H1, H2, H3, D_out)
+X_test_tensor = torch.tensor(X_test, dtype=torch.float)
+Y_test_tensor = torch.tensor(y_test, dtype=torch.float)
+
+def calculate_accuracy(model):
+    # Forward and backward passes
+    with torch.no_grad():
+        output = model.forward(X_test_tensor)
+
+    print("output:", output[0:3])
+    predicted_labels = torch.argmax(output, dim=1).apply_(lambda x: labels[x])
+    print("Y_test_tensor", Y_test_tensor[0:3])
+    print("predicted_labels", predicted_labels[0:3])
+    accuracy = sum(Y_test_tensor == predicted_labels)/len(Y_test_tensor)
+    accuracy = accuracy.item()
+    return accuracy
+
+input_size, output_size = X_train_tensor.shape[1], Y_train_tensor.shape[1]
+
+model = Net(input_size, H1, H2, H3, output_size)
 criterion = nn.MSELoss(reduction='sum')
-optimizer = torch.optim.Adam(model1.parameters(), lr=1e-4 * 2)
+optimizer = torch.optim.Adam(model.parameters(), lr=1e-4 * 2)
 
-losses1 = []
+losses = []
+accuracies = []
+iterations = 10
+iterations_to_accuracy = 1
+iterations_to_save = iterations_to_accuracy
 
-for t in range(10):
-    y_pred = model1(numeric_x)
+for t in range(iterations):
+    y_pred = model(X_train_tensor)
     
-    loss = criterion(y_pred, numeric_y)
+    loss = criterion(y_pred, Y_train_tensor)
     print(t, loss.item())
-    losses1.append(loss.item())
+    losses.append(loss.item())
+
+    if t!=0 and t % iterations_to_accuracy == 0:
+        accuracy = calculate_accuracy(model)
+        print("accuracy", accuracy)
+        accuracies.append(accuracy)
+
+    if t!=0 and t % iterations_to_save == 0:    
+        torch.save(model, f"models/model {t} accuracy {accuracy}.pt")
     
     if torch.isnan(loss):
         break
@@ -77,5 +112,7 @@ for t in range(10):
     loss.backward()
     optimizer.step()
 
-plt.plot(range(len(losses1)), losses1)
+plt.plot(range(len(losses)), losses)
 plt.savefig("loss.png")
+
+torch.save(model, "final_model.pt")
